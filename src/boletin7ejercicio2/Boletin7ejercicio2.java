81ad64d3ecc414b1c01df95c266cb429875df412
antonio <antonio@192.168.0.20>
2015-11-02 22:05:42 +0100

    boletin7ejercicio2

---
+++ src/boletin7ejercicio2/Boletin7ejercicio2.java
@@ -0,0 +1,19 @@
+
+package boletin7ejercicio2;
+
+
+public class Boletin7ejercicio2 {
+
+
+    public static void main(String[] args) {
+        short num1 = 5;
+        short num2 = 3;
+       System.out.println("introduce un numero + visualizar la suma");
+       if(num1>num2)
+           System.out.print("el numero es positivo");
+       else
+           System.out.print("el numero es negativo");
+    }
+    
+    
+}
